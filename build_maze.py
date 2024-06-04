708d8739c583237a7077d1aa79d3658e7f449aa7
Alex Petitjean <alex.m.petitjean@gmail.com>
2024-06-03 22:51:06 -0500

    Optimize plant_maze()
    No need to loop and plant bushes everywhere, one is enough.

--- build_maze.py
+++ build_maze.py
@@ -1,17 +1,14 @@
 from fertilizing import fertilize
-from move_util import goto, world_range
+from move_util import goto
 
 def plant_maze():
+    goto(0)
     if get_entity_type() == Entities.Hedge:
         return
 
-    for i in world_range():
-        goto(i)
-        if get_entity_type() != Entities.Bush and can_harvest():
-            harvest()
-        if get_ground_type() != Grounds.Turf:
-            till()
-        plant(Entities.Bush)
+    if get_ground_type() != Grounds.Turf:
+        till()
+    plant(Entities.Bush)
 
     while get_entity_type() != Entities.Hedge:
         fertilize()
